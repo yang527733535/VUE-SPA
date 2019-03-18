f191c58ff2b9129a336340cd29be8dae01e6989a
yang527733535 <15817257525@163.co>
2019-03-18 18:40:40 +0800

    1

--- src/components/photos/Photolist.vue
+++ src/components/photos/Photolist.vue
@@ -1,35 +1,51 @@
 <template>
     <div>
      <div id="slider" class="mui-slider ">
-			<div class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
-    <div class="mui-scroll">
-        <a class="mui-control-item mui-active">
-            推荐
-        </a>
-        <a class="mui-control-item">
-            热点
-        </a>
-        <a class="mui-control-item">
-            北京
-        </a>
-        <a class="mui-control-item">
-            社会
-        </a>
-        <a class="mui-control-item">
-            娱乐
-        </a>
-        <a class="mui-control-item">
-            科技
-        </a>
-    </div>
-</div>
+				<div id="sliderSegmentedControl" class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
+					<div class="mui-scroll">
+						<a class="mui-control-item mui-active" href="#item1mobile" data-wid="tab-top-subpage-1.html">
+							推荐
+						</a>
+						<a class="mui-control-item" href="#item2mobile" data-wid="tab-top-subpage-2.html">
+							热点
+						</a>
+						<a class="mui-control-item" href="#item3mobile" data-wid="tab-top-subpage-3.html">
+							北京
+						</a>
+						<a class="mui-control-item" href="#item4mobile" data-wid="tab-top-subpage-4.html">
+							社会
+						</a>
+						<a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
+							娱乐
+						</a>
+						<a class="mui-control-item" href="#item6mobile" data-wid="tab-top-subpage-6.html">
+							科技
+						</a>
+					</div>
+				</div>
 
 			</div>
+	
     </div>
 </template>
 <script>
+//1导入mui 的js文件
+// import mui from '../../lib/mui/js/mui.min.js'
+//2 初始化滑动控件
+//  mui('.mui-scroll-wrapper').scroll({
+// 	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+//     });
+
+
 export default {
-    
+    data(){
+        return {
+
+        }
+    },
+    methods:{
+
+    }
 }
 </script>
 
