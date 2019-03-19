7fac2f17df7f86b353d54a799658ba576b14263a
yang527733535 <15817257525@163.co>
2019-03-19 22:28:16 +0800

    183

--- src/components/goods/GoodsInfo.vue
+++ src/components/goods/GoodsInfo.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="goodsinfo-container">
 
+        <transition
+         @before-enter="beforeEnter"
+         @enter="enter"
+         @after-enter="afterEnter"
+        >
+         <div class="ball" v-show="ballflag"></div>
+        </transition>
+   
         
  <!-- 商品轮播图区域 -->
      <div class="mui-card">
@@ -23,10 +31,15 @@
                           市场价：￥<del>{{goodsinfo.market_price}}</del>&nbsp;&nbsp;销售价：
                           <span class="now_price">￥{{goodsinfo.sell_price}}</span>
                          </p>
-                         <p>购买数量: <numbox ></numbox> </p>
+                         <p>购买数量: <numbox :max="goodsinfo.stock_quantity" @getconut="getSeletedCount" ></numbox> </p>
                          <p>
                     <mt-button size="small" type="primary">立即购买</mt-button>
-                    <mt-button size="small" type="danger">加入购物车</mt-button>
+                  <mt-button size="small" @click='addToshopcar' type="danger">加入购物车</mt-button>
+                <!-- 分析 如何实现加入购物车时候，拿到选择的数量 -->
+                    <!-- 1 经过分析发现，按钮属于goodsinfo页面，数字属于numberbox组件
+                    2 由于涉及到了父子福建的嵌套了，所以，无法直接在goodsinfo页面中获取选中的商品数量 -->
+                    <!-- 3 怎么解决 涉及到了子传父  （时间调用机制） -->
+                    <!-- 4  事件调用的本质，父向子传递方法， 子调用这个方法，同时把数据当做参数传给这个方法 -->
                          </p>
 					</div>
 				</div>
@@ -41,7 +54,7 @@
 					<div class="mui-card-content-inner">
 					   <p>商品货号:{{goodsinfo.goods_no}}</p>
                        <p>库存情况:{{goodsinfo.stock_quantity}}件</p>
-                       <p>上架时间:{{goodsinfo.add_time|dateFormat}}</p>
+                       <p>上架时间:{{goodsinfo.add_time}}</p>
 					</div>
 				</div>
 				<div class="mui-card-footer">
@@ -66,7 +79,9 @@ export default {
             //将路由参数对象中的id挂载到data上，方便后期调用
             id:this.$route.params.id,
             lunbotu:[] ,//轮播图的数据
-            goodsinfo:{}
+            goodsinfo:{},
+            ballflag:false ,//控制小球隐藏和显示的表示
+            selecedCount:1  //保存用户选中的商品数量   认为用户买一个
         }
     },
     created(){
@@ -104,7 +119,29 @@ export default {
         },
         go2(id){
          this.$router.push({name:'pinglun',params:{id}})
+        },
+        addToshopcar(){
+            //添加到购物车
+            this.ballflag= ! this.ballflag
+        },
+        beforeEnter(el){
+            el.style.transform="translate(0,0)";
+        },
+        enter(el,done){
+            el.offsetWidth;
+            el.style.transform="translate(93px,230px)";
+            el.style.transiton="all 1s ease";
+            done();
+        },
+        afterEnter(el){
+            this.ballflag =!this.ballflag;
+        },
+        getSeletedCount(count){
+            //当子组件把选中的数量传递给父组件的时候  把选中的值保存到 data上
+        this.selecedCount =count;
+        console.log(this.selecedCount)
         }
+
     }
 }
 </script>
@@ -123,5 +160,15 @@ export default {
           button{
                margin: 15px 0;
           }
+      }
+      .ball{
+          width: 15px;
+          height: 15px;
+          border-radius:50%;
+          background-color: red;
+          position: absolute;
+          z-index: 999;
+          top: 400px;
+          left: 146px;
       }
 </style>
--- src/components/subcomponents/goodsinfo_numberbox.vue
+++ src/components/subcomponents/goodsinfo_numberbox.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-input-number size="mini" v-model="num1" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
+  <el-input-number size="mini" v-model="num1" @change="handleChange" :min="1" :max="max" label="描述文字"></el-input-number>
 </template>
 <script>
   export default {
@@ -8,9 +8,12 @@
         num1: 1
       };
     },
+    props:["max"],
+       
+   
     methods: {
       handleChange(value) {
-        console.log(value);
+         this.$emit('getconut',value)
       }
     }
   };
