0f2e12bcc8108d53875c26e42540cf61c7597e3c
yang527733535 <15817257525@163.co>
2019-03-19 20:05:38 +0800

    178

--- src/components/goods/GoodsDesc.vue
+++ src/components/goods/GoodsDesc.vue
@@ -1,15 +1,50 @@
 <template>
-    <div>
-        <h3>商品评论</h3>
+    <div class="goodsdesc-container">
+        <h3>{{info.title}}</h3>
+         <hr>
+        <div class="content" v-html="info.content"></div>
     </div>
+
+
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return {
+             info:{} //图文数据
+        }
+    },
+    created(){
+    this.getGoodsDesc();
+    },
+    methods:{
+        getGoodsDesc(){
+            this.$http.get('http://www.liulongbin.top:3005/api/goods/getdesc/'+this.$route.params.id)
+            .then(data=>{
+                if(data.body.status===0){
+                    console.log(data.body.message[0])
+                    this.info =data.body.message[0]
+                }
+            })
+        }
+    }
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss" >
+  .goodsdesc-container{
+      padding: 4px;
+      h3{
+          font-size: 16px;
+          color:#226aff;
+          text-align: center;
+          margin:15px 0;
+      }
+      .content{
+          img{
+              width: 100%;
+          }
+      }
+  }
 </style>
--- src/components/goods/Goodscomment.vue
+++ src/components/goods/Goodscomment.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
-        <h3>商品评论</h3>
+        <cmtbox :id="$route.params.id" ></cmtbox>
     </div>
 </template>
 
 <script>
+import cmtbox from '../subcomponents/comment.vue'
 export default {
-    
+    components:{cmtbox}
 }
 </script>
 
